6e61817659fef765c26167f10ad1811bad5fb311
José Matheus <XxFULLDLCxX@outlook.com>
2023-11-12 07:10:09 -0300

    build: initialize project, installing dependencies
    - Linting Setup (including ESLint, Prettier, and Husky)
    - Path Mapping
    - Health Check Route
    - Deployment Configuration
    - Error Middleware Integration
    - Testing Environment Configuration
    - Project Structure Layout
    - Documentation Setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 'ESNext',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', 
+    'plugin:import/recommended', 
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/order': 'warn',
+    'import/no-named-as-default-member': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/newline-after-import': ['error'],
+    'lines-between-class-members': ['error', 'always'],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': { typescript: {}, node: {} },
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Bet-Back
-Projeto #23: Desafio Técnico - Quer apostar quanto? (back-end)
+
+#### Deploy: https://bet-back-oahk.onrender.com
+---
+Crie um `.env.development` & `.env.test` seguindo o `.env.example`
+___
+#### Instale as Dependências:
+```bash 
+npm i
+```
+---
+<details>
+  <summary>Sobre os Testes</summary>
+
+  #### Para rodar os testes automatizados:
+  ```bash
+  npm run test
+  ```
+  #### Para rodar os testes de uma feature específica:
+  ```bash
+  npm run test feature-name
+  ```
+</details>
+
+---
+#### Para iniciar o Servidor local: 
+```bash 
+npm run dev
+```
+---
